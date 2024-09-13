10a42b7aa5179ed6da6578ac6a16972b02abedcc
Ken Fukuyama <kenfdev@gmail.com>
2024-09-13 08:58:53 +0000

    chore: add interaction test

--- src/Form.stories.tsx
+++ src/Form.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { http, HttpResponse } from 'msw';
+import { userEvent, waitFor, within, expect, fn } from '@storybook/test';
 
 import { Form } from './Form';
 
@@ -22,11 +22,30 @@ const meta: Meta<typeof Form> = {
     },
   },
   args: {
-    onSubmit: action('submit'),
+    onSubmit: fn(),
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof Form>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({ args, canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step('select option 2', async () => {
+      await waitFor(() =>
+        expect(canvas.getByRole('combobox')).toHaveTextContent('Option 1')
+      );
+      await userEvent.selectOptions(canvas.getByRole('combobox'), '2');
+    });
+
+    await step('submit form', async () => {
+      await userEvent.click(canvas.getByRole('button', { name: 'Submit' }));
+    });
+
+    await waitFor(() =>
+      expect(args.onSubmit).toHaveBeenCalledWith({ option: '2' })
+    );
+  },
+};
